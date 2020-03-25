74c78e20b5340524f720beaf78ff2c61cb1d419e
Alexandre <alexandre.berthiot@sefam-medical.com>
2020-03-25 11:18:39 +0100

    first shot to edit gatsby starter. wip not found page

--- src/pages/404.js
+++ src/pages/404.js
@@ -10,8 +10,12 @@ const NotFoundPage = ({ data, location }) => {
   return (
     <Layout location={location} title={siteTitle}>
       <SEO title="404: Not Found" />
-      <h1>Not Found</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      <h1>404: Page Not Found</h1>
+      <p>You enter into a dead-end route, nothing usually fears you, but something's wrong about this place ... What do you want to do ?</p>
+      <div>
+        <button >Head back, this place is creepy</button>
+        <button >Take me home !</button>
+      </div>
     </Layout>
   )
 }
